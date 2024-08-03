9ce724994e168b1d317b85a8d07a9fb71cfe630f
Dimitar Dimitrov <idimitak@gmail.com>
2024-08-04 00:50:55 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col space-y-4 items-center justify-center"></main>
+    <main className="flex min-h-screen flex-col space-y-4 items-center justify-center">
+      <Link href="/about">About</Link>
+      <Link href="/gallery">Gallery</Link>
+    </main>
   );
 }
